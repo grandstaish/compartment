52b368fcd672ba0dba6b50e29de683211dc7d287
Christoph Hennemann <christoph.hennemann@gmail.com>
2016-02-07 17:15:07 +0100

    feat: Make retained Presenter instance visible in UI
    Added TextView to show the Memory location of the Presenter.
    It is used as an addition to the logcat print for convenience.

--- sample/src/main/java/nz/bradcampbell/sample/MyFragment.java
+++ sample/src/main/java/nz/bradcampbell/sample/MyFragment.java
@@ -7,20 +7,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.TextView;
 import nz.bradcampbell.compartment.PresenterControllerFragment;
 
 public class MyFragment extends PresenterControllerFragment<MyPresenterComponent, MyPresenter> implements MyView {
+
+    private TextView mContent;
+
     @Override protected MyPresenterComponent onCreateNonConfigurationComponent() {
         return new MyPresenterComponent();
     }
 
     @Nullable @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.my_layout, container, false);
+        View rootView = inflater.inflate(R.layout.my_layout, container, false);
+        mContent = ((TextView) rootView.findViewById(R.id.content));
+        return rootView;
     }
 
     @Override public void doSomething() {
         MyPresenter p = getPresenter();
+        mContent.setText(p.toString());
 
         // Print the memory location in logcat so see if it is the same instance
         // on config changes
